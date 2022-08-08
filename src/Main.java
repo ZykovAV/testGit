86a1ef4ddd66f87213f5a8ce0e1015772db4d0c0
ZykovAV <cah34ka@gmail.com>
2022-08-09 00:45:55 +0300

    Коммит первого задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+    }
+    private static void task1() {
+        System.out.println("Решение первой задачи");
     }
 }
